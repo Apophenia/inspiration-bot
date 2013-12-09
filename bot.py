798e8de13c6059fbe2438559c53b19d938657a36
Lyndsey <lyndsey@lyndseyjane.com>
2013-12-08 20:53:32 -0500

    Added word-phrase relationship density table for Markov chaining

--- bot.py
+++ bot.py
@@ -1,5 +1,8 @@
 import random
 import zulip
+import string
+from itertools import izip
+from collections import defaultdict
 
 client=zulip.Client()
 
@@ -14,4 +17,12 @@ def respond(message):
             'content': 'Dear ' + message['sender_full_name'] + ',\r\n' + str(random.choice(flines))
         })
 
+def make_table(text):
+    parsed = string.split(text)
+    table = defaultdict(list)
+    for a, b, c in izip(parsed, parsed[1:], parsed[2:]):
+        table[(a,b)].append(c)
+    return table
+
+print make_table("i love pairing with zach and also mary and also alan!")
 client.call_on_each_message(respond)
